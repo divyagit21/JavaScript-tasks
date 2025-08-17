3f1f8679e6dd100572d72707f1e7d230be2faceb
divyagit21 <mittasaidivya21@gmail.com>
2025-08-17 13:26:30 +0530

    first commit

--- tasks.js
+++ tasks.js
@@ -2,7 +2,7 @@ export const tasks = [
   {
     title: "Drums",
     live: "./task1/index.html",
-    code: "https://github.com/your-username/js-calculator"
+    code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task1"
   },
   {
     title: "Gradient Background Generator",
@@ -31,107 +31,107 @@ export const tasks = [
   },
   {
     title: "Scratch card",
-    live: "./task7/index.html",
+    live: "./task8/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task8"
   },
   {
     title: "Tic Tac Toe",
-    live: "./task8/index.html",
+    live: "./task9/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task9"
   },
   {
     title: "Cart",
-    live: "./task9/index.html",
+    live: "./task10/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task10"
   },
   {
     title: "Search",
-    live: "./task10/index.html",
+    live: "./task11/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task11"
   },
   {
     title: "Search Highlighter",
-    live: "./task11/index.html",
+    live: "./task12/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task12"
   },
   {
     title: "Color Picker",
-    live: "./task12/index.html",
+    live: "./task13/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task13"
   },
   {
     title: "Cart Filter",
-    live: "./task13/index.html",
+    live: "./task14/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task14"
   },
   {
     title: "Tab switch",
-    live: "./task14/index.html",
+    live: "./task15/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task15"
   },
   {
     title: "Progress",
-    live: "./task15/index.html",
+    live: "./task16/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task16"
   },
   {
     title: "Picture Information",
-    live: "./task16/index.html",
+    live: "./task17/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task17"
   },
   {
     title: "Event Key Code",
-    live: "./task17/index.html",
+    live: "./task18/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task18"
   },
   {
     title: "Tool Tip",
-    live: "./task18/index.html",
+    live: "./task19/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task19"
   },
   {
     title: "Infinite Loading",
-    live: "./task19/index.html",
+    live: "./task20/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task20"
   },
   {
     title: "Arrow Toggle",
-    live: "./task20/index.html",
+    live: "./task21/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task21"
   },
   {
     title: "Dynamic Scroll",
-    live: "./task21/index.html",
+    live: "./task22/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task22"
   },
   {
     title: "Car Game",
-    live: "./task22/index.html",
+    live: "./task23/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task23"
   },
   {
     title: "Random Ball",
-    live: "./task23/index.html",
+    live: "./task24/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task24"
   },
   {
     title: "Mouse Sprinkles",
-    live: "./task24/index.html",
+    live: "./task25/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task25"
   },
   {
     title: "Canvas",
-    live: "./task25/index.html",
+    live: "./task26/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task26"
   },
   {
     title: "Tags Input",
-    live: "./task26/index.html",
+    live: "./task27/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task27"
   },
   {
     title: "Alert Toaster",
-    live: "./task27/index.html",
+    live: "./task28/index.html",
     code: "https://github.com/divyagit21/JavaScript-tasks/tree/master/task28"
   },
 ];
